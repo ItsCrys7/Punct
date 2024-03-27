5f1e74503d0ca586fb0b813f6266af2f93a73226
Crys <cristian.toanca@emanuel.ro>
2024-03-27 15:47:51 +0200

    tema punct

---
+++ PunctColorat.java
@@ -0,0 +1,9 @@
+public class PunctColorat extends Punct{
+    
+    String culoare;
+    public PunctColorat(int x, int y, String Culoare){
+        super(x, y);
+        this.culoare = culoare;
+    }
+
+}
